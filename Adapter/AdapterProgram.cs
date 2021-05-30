5bc42c1d9df4613bfb6df29c8a3f718002dc3618
RedRabel99 <lyskopawel03@gmail.com>
2021-05-30 22:56:30 +0200

    Added Adapter pattern example

---
+++ Adapter/AdapterProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Adapter
+{
+    public interface AndroidPhone
+    {
+        public void PlayGame();
+    }
+
+    public class GameBoy
+    {
+        public void PlayPokemonFireRed()
+        {
+            Console.WriteLine("Starting Pokémon FireRed...");
+            Console.WriteLine("Hello there! Welcome to the world of Pokémon! " +
+                              "My name is Oak! People call me the Pokémon Prof!\n" +
+                              "This world is inhabited by creatures called Pokémon!" +
+                              " For some people, Pokémon are pets. Other use them for fights.");
+        }
+    }
+
+    public class Emulator : AndroidPhone
+    {
+        private GameBoy _gameBoy;
+
+        public Emulator(GameBoy gameBoy)
+        {
+            _gameBoy = gameBoy;
+        }
+
+        public void PlayGame()
+        {
+            _gameBoy.PlayPokemonFireRed();
+        }
+    }
+    class AdapterProgram
+    {
+        static void Main(string[] args)
+        {
+            GameBoy gameBoy = new GameBoy();
+            Emulator emulator = new Emulator(gameBoy);
+            
+            emulator.PlayGame();
+        }
+    }
+}
